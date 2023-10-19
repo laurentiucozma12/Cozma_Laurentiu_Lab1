7667c0c7b96983c1c0a87389e03a654d6e9c15da
Lau <laurentiucozma12@gmail.com>
2023-10-19 16:45:40 +0300

    Laborator 1 done

---
+++ Controllers/MyCustomController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cozma_Laurentiu_Lab1.Controllers
+{
+    public class MyCustomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Welcome to the custom controller!");
+        }
+
+        public IActionResult GreetUser()
+        {
+            string greet = "Hello user!";
+
+            return Content(greet);
+        }
+
+        public IActionResult DisplayMessage(string message, int number)
+        {
+            message = "We have";
+            number = 5;
+
+            string result = $"{message} {number} cookies";
+
+            return Content(result);
+        }
+    }
+}
